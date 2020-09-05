abf808edc7c8089eeebe3a0389f3ea5afcfd4ec6
Vipin kumar <Vipin.kumar@marketcube.com>
2020-09-05 13:37:22 +0530

    Editing the README to try out git add/commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,38 +2,39 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router'
 import { HomeComponent } from './home/home.component';
-import { AddProductComponent } from './add-product/add-product.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LayoutComponent } from './layout/layout.component';
 import { ProductComponent } from './product/product.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
-import { AboutusComponent } from './aboutus/aboutus.component';
-import { ContactComponent } from './contact/contact.component';
+import { AddProductComponent } from './add-product/add-product.component';
 
-const routes: Routes =[
-  {path:'', component:HomeComponent},
-  {path:'about', component:AboutusComponent},
-  {path:'contact', component:ContactComponent},
-  {
-    path:'product',
-    children:[
-      {
-        path:'add', component:AddProductComponent
-      },
-      {
-        path:'details', component:ProductComponent
-      },
-      {
-        path:'list', component:ProductListComponent
-      },
-    ]
-  },
-  {path:'**', component:PagenotfoundComponent}
+
+const routes: Routes =[{
+  path: '' , component: HomeComponent
+},
+{
+  path: 'aboutus' , component: AboutUsComponent
+},
+{
+  path: 'product',
+  children: [
+    {
+      path: 'productList', component: ProductComponent
+    },
+    {
+      path: 'addProduct', component: AddProductComponent
+    }
+  ]
+},
+{
+  path: '**' , component: PageNotFoundComponent
+}
 ];
 
 
 @NgModule({
   imports: [
-    [RouterModule.forRoot(routes)]
+    (RouterModule.forRoot(routes))
   ],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
-import { AboutusComponent } from './aboutus/aboutus.component';
-import { ContactComponent } from './contact/contact.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 import { ProductComponent } from './product/product.component';
-import { ProductListComponent } from './product-list/product-list.component';
 import { AddProductComponent } from './add-product/add-product.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { LayoutComponent } from './layout/layout.component';
-import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
  
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutusComponent,
-    ContactComponent,
+    AboutUsComponent,
     ProductComponent,
-    ProductListComponent,
     AddProductComponent,
-    LayoutComponent,
-    PagenotfoundComponent 
+    PageNotFoundComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
